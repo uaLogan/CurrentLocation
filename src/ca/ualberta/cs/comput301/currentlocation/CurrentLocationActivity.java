a28b06b142b8b86307815dd9ee60b024391c86bc
uaLogan <lglin@cs.ualberta.ca>
2014-02-04 19:12:15 -0700

    It's working

--- src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
+++ src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cs.comput301.currentlocation;
 
+import java.util.Date;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,26 +12,63 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class CurrentLocationActivity extends Activity {
+	
 	private static final String GPS_MOCK_PROVIDER = "GpsMockProvider";
+	
+	protected TextView locTextView;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		locTextView = (TextView) findViewById(R.id.myLocationText);
 		
 		// Obtain LocationManager service and set up location update request.
-		// To Do
+		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
 	}
-
 	
 	// Retrieve location updates through LocationListener interface
-	private final LocationListener locationListener = new LocationListener(){
-	// To Do: override the four methods.
-	}	
+	protected final LocationListener locationListener = new LocationListener()
+	{
+		@Override
+		public void onLocationChanged(Location location)
+		{
+			if(location != null)
+			{
+				double lat = location.getLatitude();
+				double lng = location.getLongitude();
+				Date date = new Date(location.getTime());
+				locTextView.setText(String.format("Your location is:\nLatitude: %f\nLongitude: %f\nat: %s", lat, lng, date.toString()));
+			}
+			else
+				locTextView.setText("Could not determine location");
+		}
 
-	
+		@Override
+		public void onProviderDisabled(String provider)
+		{
+
+		}
+
+		@Override
+		public void onProviderEnabled(String provider)
+		{
+
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras)
+		{
+
+		}		
+	};
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
